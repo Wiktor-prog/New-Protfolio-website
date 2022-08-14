8f87e67cba48a2d915e92a423f443a067155a609
Wiktor Ziejewski <wiktor.ziejewski>
2022-08-14 18:50:06 +0200

    Contact section update

--- src/components/Contact/ContactChoise/ContactChoise.js
+++ src/components/Contact/ContactChoise/ContactChoise.js
@@ -10,35 +10,30 @@ export default function ContactChoise() {
     <div className="contact-list">
       <ul>
         <li>
-            <a href="https://wa.me/505333789" target="_blank">
-
+            <a 
+            href="https://wa.me/505333789" 
+            target="_blank">
                 <span>
                   <FaWhatsappSquare/>
                 </span>
-
-                <p>Whatsapp</p>
-               
+                <p>Whatsapp</p> 
             </a>
         </li>
     
         <li>
             <a href="https://www.linkedin.com/in/wiktor-ziejewski-43b16b229/">
-
                 <span>
                   <FaLinkedin/>
                 </span>
-
               <p>Linkedin</p>
             </a>
         </li>
 
         <li>
             <a href="mailto:wiktor.ziejewski@gmail.com">
-
                 <span>
                   <FaEnvelope/>
                 </span>
-
             <p>E-mail</p>
           </a>
         </li>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,22 +4,30 @@ import "../Navbar/Navbar.css"
 export default function Navbar() {
   return (
   
-  <nav>
-      <ul className="menu_top">
-        <li>
-          <a class="nav_Btns" href="/about">About me</a>
-        </li>
+    <nav>
+        <ul className="menu_top">
 
-        <li>
-          <a class="nav_Btns" href="https://github.com/Wiktor-prog">GitHub</a>
-        </li>  
-        
-        <li>
-          <a class="nav_Btns" href="/contact">Contact</a>
-        </li>
-      </ul>
-  </nav>
-  
+          <li>
+            <a 
+            class="nav_Btns" 
+            href="/about">About me</a>
+          </li>
+
+          <li>
+            <a 
+            class="nav_Btns" 
+            href="https://github.com/Wiktor-prog">GitHub</a>
+          </li>  
+          
+          <li>
+            <a 
+            class="nav_Btns" 
+            href="/contact">Contact</a>
+          </li>
+          
+        </ul>
+    </nav>
+
 
   )
 }
