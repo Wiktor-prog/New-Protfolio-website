71ac5ce25f8059388bb300db310ebf86aa8c1473
Wiktor Ziejewski <wiktor.ziejewski>
2022-08-19 19:49:40 +0200

    create about template section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
 import Contact from './components/Contact/Contact'
 
 export default function App() {
   return (
     <div className="App">
+      
       <Navbar />
         <BrowserRouter>
           <Routes>
             <Route path="/contact" element={<Contact/>} />
+            <Route path="/About" element={<About/>} />
           </Routes>
         </BrowserRouter>
         
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "../About/About.css"
 
 export default function About() { 
   return(
-    <main>
-      <div className="contenet">
-        <h1>About me</h1>
-      </div>
-    </main>
+    <div className="about-page">
+      <header className="about-header">aaa</header>
+        <main className="about-grid">
+          <div className="about-cell">
+            <h3>Skills</h3>
+          </div>
+
+          <div className="about-cell">
+            <h3>Skills</h3>
+          </div>
+
+          <div className="about-cell">
+            <h3>Skills</h3>
+          </div>
+        </main>
+    </div>
   )
 }
