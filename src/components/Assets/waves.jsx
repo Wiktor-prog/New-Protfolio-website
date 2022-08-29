abb31e9264afb2e588a9677568f0152adff66144
Wiktor Ziejewski <wiktor.ziejewski>
2022-08-29 08:02:27 +0200

    add RWD

--- src/components/Assets/waves.jsx
+++ src/components/Assets/waves.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Wave from "react-wavify"
+import "../Assets/waves.css"
 
 export default function wave() {
   return (
-    
+
     <Wave
         fill="#e9328d66"
         className="wave"
         paused={false}
         options={{
-          height: 50,
-          amplitude: 30,
+          height: 80,
+          amplitude: 20,
           speed: .14,
           points: 3,
           opacity: 0.9,
         }}
     />
+
    
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Button/>
 
       </div> 
-        <Wave/>
+      <Wave/>
   </div>
 
   )
